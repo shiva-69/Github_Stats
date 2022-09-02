c3b53f994eadb4e34a802833db52eb0c209ed7c8
Shivam Pandey <shivam8kp@gmail.com>
2022-09-03 01:39:35 +0530

    added search repo by time functionality

--- src/Components/RepoContainer.jsx
+++ src/Components/RepoContainer.jsx
@@ -7,22 +7,117 @@ export const RepoContainer = () => {
     const [data, setData] = React.useState([]);
     const [loading, setLoading] = React.useState(true);
     const [error, setError] = React.useState(false);
+    const [date, setDate] = React.useState("2022-09-01");
+    const ModifiedDate = ( year, month, day) =>{
+        return new Date(
+            new Date().getFullYear() - year,
+            new Date().getMonth() - month , 
+            new Date().getDate() - day
+        );        
+    }
+    
+    const handleChange = (e) => {
+        if(e.target.value == "1week"){
+            let newDate = ModifiedDate(0, 0, 7)
+            let month = newDate.getMonth();
+            month = month.toString();
+            if(month.length == 1){
+                month = "0" + month
+                console.log(month)
+            }
+            let day = newDate.getDate();
+            day = day.toString();
+            if(day.length == 1){
+                day = "0" + day
+                console.log(day)
+            }
+            setDate(`${newDate.getFullYear()}-${month}-${day}`);
+        }
+        else if(e.target.value == "2week"){
+            let newDate = ModifiedDate(0, 0, 14)
+            let month = newDate.getMonth();
+            month = month.toString();
+            if(month.length == 1){
+                month = "0" + month
+                console.log(month)
+            }
+            let day = newDate.getDate();
+            day = day.toString();
+            if(day.length == 1){
+                day = "0" + day
+                console.log(day)
+            }
+            setDate(`${newDate.getFullYear()}-${month}-${day}`);
+        }
+        else if(e.target.value == "1month"){
+            let newDate = ModifiedDate(0, 1, 0)
+            let month = newDate.getMonth();
+            month = month.toString();
+            if(month.length == 1){
+                month = "0" + month
+                console.log(month)
+            }
+            let day = newDate.getDate();
+            day = day.toString();
+            if(day.length == 1){
+                day = "0" + day
+                console.log(day)
+            }
+            setDate(`${newDate.getFullYear()}-${month}-${day}`);
+        }
+        else if(e.target.value == "6month"){
+            let newDate = ModifiedDate(0, 6, 0)
+            let month = newDate.getMonth();
+            month = month.toString();
+            if(month.length == 1){
+                month = "0" + month
+                console.log(month)
+            }
+            let day = newDate.getDate();
+            day = day.toString();
+            if(day.length == 1){
+                day = "0" + day
+                console.log(day)
+            }
+            setDate(`${newDate.getFullYear()}-${month}-${day}`);
+        }
+        else if(e.target.value == "1year"){
+            let newDate = ModifiedDate(1, 0, 0)
+            let month = newDate.getMonth();
+            month = month.toString();
+            if(month.length == 1){
+                month = "0" + month
+                console.log(month)
+            }
+            let day = newDate.getDate();
+            day = day.toString();
+            if(day.length == 1){
+                day = "0" + day
+                console.log(day)
+            }
+            setDate(`${newDate.getFullYear()}-${month}-${day}`);
+        }
+    }
     React.useEffect(()=>{
-        fetch("https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc")
+        setLoading(true)
+        console.log(`https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc`)
+        fetch(`https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc`)
         .then((res) => res.json())
         .then((res) => {
             setData(res.items)
-            setLoading(false);
         })
-        .catch((err) => setError(true));
-    }, [])
+        .catch((err) => setError(true))
+        .finally(() => setLoading(false))
+    }, [date])
     return <> 
     <Flex justify="center" align="center" gap="1%">
         <Heading as='h5' size='sm'> Select Time</Heading> 
-        <Select placeholder='Select option' w="100px">
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
+        <Select  w="150px" onChange={handleChange} placeholder="Select an option">
+            <option value='1week'>1 week</option>
+            <option value='2week'>2 week</option>
+            <option value='1month'>1 month</option>
+            <option value='6month'>6 month</option>
+            <option value='1year'>1 Year</option>
         </Select>
     </Flex>
         {
